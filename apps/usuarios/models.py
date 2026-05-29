6a175553257ca7da58d1647a8f7ae49ed6b3359a
diegol447 <diegoamoreira33@gmail.com>
2026-05-29 19:41:16 -0300

    models/usuarios

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 
+
 class Usuario(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    email = models.EmailField('E-mail', unique=True)
-    telefone = models.CharField('Telefone', max_length=20)
-    cpf = models.CharField('CPF', max_length=14, unique=True)
-    data_nascimento = models.DateField('Data de Nascimento')
-    ativo = models.BooleanField('Ativo', default=True)
-    criado_em = models.DateTimeField('Criado em', auto_now_add=True)
+    nome = models.CharField(
+        verbose_name='Nome',
+        max_length=100,
+    )
+    email = models.EmailField(
+        verbose_name='E-mail',
+        unique=True,
+    )
+    telefone = models.CharField(
+        verbose_name='Telefone',
+        max_length=20,
+    )
+    cpf = models.CharField(
+        verbose_name='CPF',
+        max_length=14,
+        unique=True,
+    )
+    data_nascimento = models.DateField(
+        verbose_name='Data de Nascimento',
+    )
+    ativo = models.BooleanField(
+        verbose_name='Ativo',
+        default=True,
+    )
+    criado_em = models.DateTimeField(
+        verbose_name='Criado em',
+        auto_now_add=True,
+    )
 
     class Meta:
         verbose_name = 'Usuário'
